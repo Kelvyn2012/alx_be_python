8bc751d1699088247ca5eeb41ae7a1a3d4012cfc
kelvin udufo <kudufo@gmail.com>
2025-05-19 22:59:52 +0100

    adding python basic calculator

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+add_result = number1 + number2
+sub_result = number1 - number2
+multiply_result = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {add_result}")
+print(f"Subtraction of {number1} and {number2} is {sub_result}")
+print(f"Multiplication of {number1} and {number2} is {multiply_result}")
