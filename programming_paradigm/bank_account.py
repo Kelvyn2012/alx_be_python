339d2956e2441e0689cb50458b3fe8f6d5d941fc
kelvin udufo <kudufo@gmail.com>
2025-06-12 21:52:16 +0100

    crated a basic deposit and withdrawer

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount():
+    def __init__(self, initial_balance=0):
+        self.initial_balance = initial_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.initial_balance  += amount
+        else:
+            print("Deposit amount must be positive.")
+    def withdraw(self, amount):
+        if amount <= 0:
+            return False
+        if amount > self.initial_balance:
+            return False
+        self.initial_balance -= amount
+        return True
+        
+    def display_balance(self):
+        print(f"Current balance: {self.initial_balance}")
