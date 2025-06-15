3d5abe250f96a2776e259662ef3c992b69947625
kelvin udufo <kudufo@gmail.com>
2025-06-15 19:44:40 +0100

    created a unittest for a simple claculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,13 +2,30 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimple_calculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    
+    
     def test_add(self):
-        calc = SimpleCalculator()
-        result = calc.add(3, 5)
-        self.assertEqual(result, 8)
+        self.assertEqual(self.calc.add(4,2),6)
+        
+    def test_subtract(self):
+        result = self.calc.subtract(5,3)
+        self.assertEqual(result,2)
 
+    def test_multiply(self):
+        result = self.calc.multiply(2,4)
+        self.assertEqual(result,8)
+        
+  
+    def test_divide_by_zero(self):
+        result = self.calc.divide(4,0)
+        self.assertIsNone(result,)    
     
-    
+        
+    def test_divide(self):
+        result = self.calc.divide(4,2)
+        self.assertEqual(result,2)
     
     
     
