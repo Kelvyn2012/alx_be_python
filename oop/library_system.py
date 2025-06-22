996b662fa62cd278a2d02d4b6682a516b1144068
kelvin udufo <kudufo@gmail.com>
2025-06-22 23:45:55 +0100

    uodated

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,16 +13,17 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self) -> str:
-        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 
 
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
-
+        
     def __str__(self) -> str:
-        return f"PrintBook: The {self.title} by {self.author}, Page Count: {self.page_count} pages"
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
